5d5a3980cb5f10022ed04a699b043afafb9d706c
Gabriel Linhares <linhares.gs96@gmail.com>
2023-11-18 16:55:19 -0300

    aula02

---
+++ Entra21/aula_01/exercicios/funcoes/static/js/aula_01.js
@@ -0,0 +1,20 @@
+// function checkAge(age) {
+//     if (age > 18) {
+//       return true;
+//     } else {
+//       return confirm('Did parents allow you?');
+//     }
+//   }
+
+//   checkAge(20)
+
+// let sum = (a, b) => a + b;
+
+// /* This arrow function is a shorter form of:
+
+// let sum = function(a, b) {
+//   return a + b;
+// };
+// */
+
+// alert( sum(1, 2) ); // 3
---
+++ Entra21/aula_02/static/js/aula_02.js
@@ -0,0 +1,10 @@
+let user = {
+    name: "John",
+    age: 30,
+}
+
+// let key = prompt("What do you want ot know about the user?")
+let key = "name"
+
+alert(user.key = "maria")
+alert(user[key])
