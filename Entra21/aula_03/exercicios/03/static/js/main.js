7d8b70fd90db0fcc1f5fcf904ce5d548ef1ecee9
Gabriel Linhares <linhares.gs96@gmail.com>
2023-11-19 13:18:25 -0300

    aula03: exercicio_03

---
+++ Entra21/aula_03/exercicios/03/static/js/main.js
@@ -0,0 +1,18 @@
+const $decreaser = $(".decreaser")
+const $increaser = $(".increaser")
+let $counter = $(".counter")
+
+decreaserCounter = () => {
+    console.log($counter)
+    $counter.text(+$counter.text() -1)
+}
+
+increaserCounter = () => {
+    console.log($counter)
+    $counter.text(+$counter.text() + 1)
+}
+
+
+$decreaser.click(decreaserCounter)
+$increaser.click(increaserCounter)
+
